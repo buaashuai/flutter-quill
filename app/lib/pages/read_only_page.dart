170640bef393ae0696b7c69c9f1e5be7e8611668
weslywang <weslywang@tencent.com>
2021-03-24 01:06:15 +0800

    example

--- app/lib/pages/read_only_page.dart
+++ app/lib/pages/read_only_page.dart
@@ -9,7 +9,7 @@ import 'dart:io' as io;
 import 'package:flutter_quill/models/documents/document.dart';
 import 'package:flutter_quill/models/documents/nodes/line.dart';
 import 'package:flutter_quill/models/documents/nodes/node.dart';
-import 'package:flutter_quill/models/documents/nodes/leaf.dart';
+import 'package:flutter_quill/models/documents/nodes/leaf.dart' hide Text;
 import 'package:flutter_quill/models/documents/nodes/block.dart';
 import 'package:flutter_quill/widgets/default_styles.dart';
 import 'package:flutter_quill/widgets/text_line.dart';
@@ -47,14 +47,11 @@ class _ReadOnlyPageState extends State<ReadOnlyPage> {
       case 'image':
         String imageUrl = node.value.data;
         return imageUrl.startsWith('http')
-            ? Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  color: Colors.green,
-                  width: 30,
-                  height: 30,
-                  child: Image.network(imageUrl),
-                ),
+            ? Container(
+                color: Colors.green,
+                width: 30,
+                height: 30,
+                child: Image.network(imageUrl),
               )
             : Image.file(io.File(imageUrl));
       default:
@@ -76,7 +73,17 @@ class _ReadOnlyPageState extends State<ReadOnlyPage> {
           styles: DefaultStyles.getInstance(context),
           embedBuilder: _defaultEmbedBuilder,
         );
+        if (node.hasEmbed) {
+          Embed embed = node.children.single as Embed;
+          // result.add(_defaultEmbedBuilder(context, embed));
+        }
+        // result.add(Text('你好2', style: TextStyle(color: Colors.red)));
+        // result.add(RichText(
+        //   text: TextSpan(text: '你好', style: TextStyle(color: Colors.red)),
+        //   textScaleFactor: MediaQuery.textScaleFactorOf(context),
+        // ));
         result.add(textLine);
+        // result.add(Align(alignment: Alignment.centerLeft, child: Container(color: Colors.blue, margin: EdgeInsets.only(right: 6, bottom: 6), child: textLine)));
       } else if (node is Block) {
         Widget ww = Align(
           alignment: Alignment.centerLeft,
@@ -114,6 +121,15 @@ class _ReadOnlyPageState extends State<ReadOnlyPage> {
         ),
         child: Wrap(
           children: _buildChildren(context),
+          // children: [
+          //   Text('你好2', style: TextStyle(color: Colors.red)),
+          //   Container(
+          //     color: Colors.green,
+          //     width: 30,
+          //     height: 30,
+          //     child: Image.network('https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png'),
+          //   )
+          // ],
         ),
       ),
     );
